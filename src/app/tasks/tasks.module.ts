4f5d2331cc2e3d31ef91529e794c2edce878a1fd
debabrata-aic <debabrata@aicnet.in>
2024-08-22 14:43:43 +0530

    fixed forms module error

--- src/app/tasks/tasks.module.ts
+++ src/app/tasks/tasks.module.ts
@@ -4,10 +4,11 @@ import { TaskComponent } from './task/task.component';
 import { TasksComponent } from './tasks.component';
 import { SharedModule } from '../shared/shared.module';
 import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [TasksComponent, TaskComponent, NewTaskComponent],
   exports: [TasksComponent],
-  imports: [CommonModule, SharedModule],
+  imports: [CommonModule, SharedModule, FormsModule],
 })
 export class TasksModule {}
